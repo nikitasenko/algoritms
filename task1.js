22de4fcd3fb1ed906adae672cffdb108ef00f723
nikitasenko <nikita.senko@mifort.org>
2018-06-14 12:45:24 +0300

    done task1

--- task1.js
+++ task1.js
@@ -1,32 +1,63 @@
-function brakets(str) {
+function testBrakets(strBrakets) {
+    if (!sumOpenCloseBrakets(strBrakets)) return 0;
+    let openBrakets = ['(', '{', '['];
+    let braketsA = ['(', ')'];
+    let braketsB = ['{', '}'];
+    let braketsC = ['[', ']'];
+    let testBrakets = [];
+    for (let i = 0; i < strBrakets.length; i++)
+    {
+        if (contains(openBrakets, strBrakets[i])) {
+            testBrakets.push(strBrakets[i]);
+        } else {
+            let lastElem = testBrakets.length-1;
+            if ( (contains(braketsA, testBrakets[lastElem]) && contains(braketsA, strBrakets[i])) ||
+                 (contains(braketsB, testBrakets[lastElem]) && contains(braketsB, strBrakets[i])) ||
+                 (contains(braketsC, testBrakets[lastElem]) && contains(braketsC, strBrakets[i])) )
+            {
+                testBrakets.pop();
+            } else return 0;
+        }
+    }
+    if (testBrakets.length === 0) return 1; else return 0;
+}
+
+function contains(arr, elem) {
+    return arr.find((i) => i === elem) != undefined;
+}
+
+function sumOpenCloseBrakets(strBrakets) {
     let brakets = ['(', ')', '{', '}', '[', ']'];
-    let a = 0;
-    let b = 0;
-    let c = 0;
-    let d = 0;
-    let e = 0;
-    let f = 0;
-    for (let i = 0; i < str.length; i++) {
-        if (str[i] === brakets[0]) a++;
-        if (str[i] === brakets[1]) b++;
-        if (str[i] === brakets[2]) c++;
-        if (str[i] === brakets[3]) d++;
-        if (str[i] === brakets[4]) e++;
-        if (str[i] === brakets[5]) f++;
+    let openA = 0;
+    let closeA = 0;
+    let openB = 0;
+    let closeB = 0;
+    let openC = 0;
+    let closeC = 0;
+    for (let i = 0; i < strBrakets.length; i++) {
+        if (strBrakets[i] === brakets[0]) openA++;
+        if (strBrakets[i] === brakets[1]) closeA++;
+        if (strBrakets[i] === brakets[2]) openB++;
+        if (strBrakets[i] === brakets[3]) closeB++;
+        if (strBrakets[i] === brakets[4]) openC++;
+        if (strBrakets[i] === brakets[5]) closeC++;
 
     }
-    if (a !== b || c !== d || e !== f) return 0;
+    if (openA !== closeA || openB !== closeB || openC !== closeC) return 0;
     else return 1;
-
 }
 
 
-let str = ['(())', '(((', '(([[{{}}]]))', '(((([[[[[]])){{})'];
+
+let str = ['(', '))', '[}', '{}', '(){}[]', '({[]}{})', '[({}())[]'];
 for (let i = 0; i < str.length; i++) {
-    if (brakets(str[i])) {
-        console.log('Все верно');
+    if (testBrakets(str[i])) {
+        console.log('YES');
     } else {
-        console.log('В строке ошибка');
+        console.log('NO');
     }
 }
+
+
+
 
